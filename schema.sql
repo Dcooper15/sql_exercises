a5ff6e242dc6c0057183810cc1fcf70b97a819e7
Dylan Cooper <dylancooper155@gmail.com>
2020-09-08 17:04:30 -0400

    added more tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,21 @@ CREATE TABLE restaurants (
     takeout boolean,
     ate_last timestamp
 );
+
+CREATE TABLE reviewer (
+    id serial PRIMARY KEY,
+    name text,
+    email varchar,
+    karma integer
+);
+
+CREATE TABLE reviews (
+    id serial PRIMARY KEY,
+    title text,
+    review text,
+    stars integer,
+    reviewer_id integer,
+    restaurant_id integer,
+    FOREIGN KEY (reviewer_id) REFERENCES reviewer (id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants (id)
+);
--- seed.sql
+++ seed.sql
@@ -1,2 +1,10 @@
 INSERT INTO restaurants (name, distance, category, stars_rating, favorite_dish, takeout, ate_last)
   VALUES ('Funky Taco', 5, 'mexican', '***', 'chicken taco', true, NULL), ('Diablo & Sons', 4, 'tapas', '***', 'chimichurra', false, NULL), ('Juniper', 3, 'pub food', '****', 'pub fries', true, NULL);
+
+
+  INSERT INTO reviewer (name, email, karma)
+    VALUES ('Dylan', 'thanks45@gmail.com', 5), ('Igor', 'igorisdope@gmail.com', 7), ('Abe', 'abeisrad@gmail.com', 3)
+
+
+INSERT INTO reviews (title, review, stars, reviewer_id, restaurant_id)
+    VALUES ('Great Food', 'The restaurant had a good ambience. I enjoyed my time here.', 4, 2, 3), ('FUN TIMES!', 'best day ever here. Excellent food.', 5, 3, 1), ('So-So', 'It was a little meh', 3, 1, 1);
